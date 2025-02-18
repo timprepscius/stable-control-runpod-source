157e72fca8a600c2b578e80dd1496d7b4e6665ef
Timothy Prepscius <timprepscius@gmail.com>
2025-02-18 08:44:21 -0500

    intermediate

--- rp_handler_sdxl_light_sdctrl.py
+++ rp_handler_sdxl_light_sdctrl.py
@@ -68,7 +68,7 @@ def process(job_id, job_input):
     output_paths = []
     for i, sample in enumerate(generated_images):
         output_name = f"{job_id}-generated-{i}"
-        output_path = f"/tmp/{output_name}.png"
+        output_path = f"/tmp/{output_name}.jpg"
         sample.save(output_path)
         output_paths.append(output_path)
 
