09676e2017dca36a2b888be418945ba51efef37f
Timothy Prepscius <timprepscius@gmail.com>
2025-02-19 14:30:35 -0500

    intermediate

--- rp_handler_sdxl_ti.py
+++ rp_handler_sdxl_ti.py
@@ -30,19 +30,21 @@ unet.load_state_dict(load_file(hf_hub_download(repo, ckpt), device=device))
 sdxl_pipe = StableDiffusionXLPipeline.from_pretrained(base, unet=unet, torch_dtype=torch.float16, variant="fp16").to(device)
 sdxl_pipe.scheduler = EulerDiscreteScheduler.from_config(sdxl_pipe.scheduler.config, timestep_spacing="trailing")
 
-print(f"SETUP ---- C {datetime.now()}");
+print(f"SETUP ---- C0 {datetime.now()}");
 
 reference_adapter = T2IAdapter.from_pretrained(
     "TencentARC/t2i-adapter-style-sdxl-1.0", 
     torch_dtype=torch.float16
 ).to(device)
 
+print(f"SETUP ---- C1 {datetime.now()}");
+
 pose_adapter = T2IAdapter.from_pretrained(
     "TencentARC/t2i-adapter-pose-sdxl-1.0", 
     torch_dtype=torch.float16
 ).to(device)
 
-print(f"SETUP ---- CD {datetime.now()}");
+print(f"SETUP ---- C2 {datetime.now()}");
 
 vae=AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
 euler_a = EulerAncestralDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
