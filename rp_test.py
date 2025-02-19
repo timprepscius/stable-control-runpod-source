19305303f8689b5ab06565309b75bc4622d011d8
Timothy Prepscius <timprepscius@gmail.com>
2025-02-19 16:00:20 -0500

    intermediate

--- rp_test.py
+++ rp_test.py
@@ -9,6 +9,6 @@ with open("test_input.json", "r") as f:
 test_input_json["id"] = "test_id"
 
 try:
-    result = process(test_input_json)
+    result = process(test_input_json['id'], test_input_json['input'])
 except Exception as e:
     traceback.print_exc()
