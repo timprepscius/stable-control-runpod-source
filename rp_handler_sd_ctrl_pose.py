a5e76bbce6ed0c615bade60a7f05bd3760116f80
Timothy Prepscius <timprepscius@gmail.com>
2025-02-20 18:15:55 -0500

    intermediate

--- rp_handler_sd_ctrl_pose.py
+++ rp_handler_sd_ctrl_pose.py
@@ -42,8 +42,8 @@ def process(env, job_id, job_input):
         image=pose_image_sized,
         width=job_input['width'],
         height=job_input['height'],
-        control_guidance_start=0.8,
-        # control_guidance_end=0.2
+        control_guidance_start=0.2,
+        control_guidance_end=0.4
     ).images
 
     output_paths = []
